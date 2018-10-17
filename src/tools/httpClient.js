3533dbddef195da17bccaa489eafb22bf491e679
ivan <iv.dovg@gmail.com>
2018-10-17 19:18:58 +0500

    isomorphic http client

---
+++ src/tools/httpClient.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import prepareApiBody from '../misc/prepareApiBody';
+import { API_SERVER } from '../constants';
+
+const instance = axios.create({
+  baseURL: API_SERVER,
+  transformResponse: [function (rawData, headers) {
+    let data;
+    if(ENV === 'server')
+      data = prepareApiBody(rawData);
+    else
+      data = rawData;
+    if(headers['content-type'].indexOf('application/json') < 0)
+      return data;
+    return JSON.parse(data);
+  }],
+})
+
+export default instance;
