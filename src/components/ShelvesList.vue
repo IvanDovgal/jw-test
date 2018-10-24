983426016f0c1a4e67eae568e2a9f5cacd11b773
ivan <iv.dovg@gmail.com>
2018-10-24 23:35:56 +0500

    shelves list

---
+++ src/components/ShelvesList.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="wrapper">
+        <ul class="list">
+            <li class="item" :key="id" v-for="{id, name} in shelves">
+                <router-link :to="{ name: 'user-bookshelf-shelf', params: { shelf: id, user }}">
+                    {{name}}
+                </router-link>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  props: ['shelves', 'user', 'current-shelf'],
+  name: 'ShelvesList',
+};
+</script>
+
+
+<style scoped>
+    .item >>> .router-link-active {
+        color: red
+    }
+
+    .wrapper {
+        text-align: center;
+    }
+
+    .list {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+    }
+
+    .item {
+        display: inline;
+        padding: 1em;
+    }
+</style>
